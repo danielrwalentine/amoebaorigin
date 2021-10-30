970aa013a5da4929c71528dce6f6c36cc8e9eefb
Daniel Wallentine <66085821+danielrwalentine@users.noreply.github.com>
2021-10-30 15:18:39 -0600

    Fixed bite corruption, changed to Manifest

--- src/main/java/me/dekuscrub/amoebaorigin/Amoebaorigin.java
+++ src/main/java/me/dekuscrub/amoebaorigin/Amoebaorigin.java
@@ -10,16 +10,12 @@ import io.github.apace100.origins.origin.OriginLayers;
 import io.github.apace100.origins.registry.ModComponents;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-
-import java.util.Objects;
 
 public class Amoebaorigin implements ModInitializer {
 
-    public static final PowerType<Power> BITE = new PowerTypeReference<Power>(new Identifier("amoeba_origin", "bite"));
+    public static final PowerType<Power> MANIFEST = new PowerTypeReference<Power>(new Identifier("amoeba_origin", "manifest"));
 
     @Override
     public void onInitialize() {
@@ -27,11 +23,10 @@ public class Amoebaorigin implements ModInitializer {
 
     public static void bitePlayer(ServerPlayerEntity player, Entity target) {
         if (player.isPlayer()) {
-            if (BITE.isActive(player)) {
+            if (MANIFEST.isActive(player)) {
                 if (target.isPlayer()) {
                     OriginLayer layer = OriginLayers.getLayer(Identifier.tryParse("origins:origin"));
                     Origin origin = ModComponents.ORIGIN.get(target).getOrigin(layer);
-                    origin.add(BITE);
                     OriginComponent component = ModComponents.ORIGIN.get(player);
                     component.setOrigin(layer, origin);
                     OriginComponent.sync(player);
