62397f81366cb27a79d7b0f15f48647ed02a8a9a
Daniel Wallentine <66085821+danielrwalentine@users.noreply.github.com>
2021-10-30 10:53:08 -0600

    Added Origins Dependency

--- src/main/java/me/dekuscrub/amoebaorigin/Amoebaorigin.java
+++ src/main/java/me/dekuscrub/amoebaorigin/Amoebaorigin.java
@@ -1,11 +1,23 @@
 package me.dekuscrub.amoebaorigin;
 
+import io.github.apace100.apoli.power.Power;
+import io.github.apace100.apoli.power.PowerType;
+import io.github.apace100.apoli.power.PowerTypeReference;
+import io.github.apace100.origins.component.OriginComponent;
+import io.github.apace100.origins.origin.Origin;
+import io.github.apace100.origins.origin.OriginLayer;
+import io.github.apace100.origins.origin.OriginLayers;
+import io.github.apace100.origins.registry.ModComponents;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 public class Amoebaorigin implements ModInitializer {
+
+    public static final PowerType<Power> BITE = new PowerTypeReference<Power>(new Identifier("amoebaorigin", "bite"));
 
     @Override
     public void onInitialize() {
@@ -13,9 +25,18 @@ public class Amoebaorigin implements ModInitializer {
 
     public static void bitePlayer(LivingEntity player, Entity target) {
         if (player.isPlayer()) {
-            player.kill();
-            target.kill();
-
+            ServerPlayerEntity pl = (ServerPlayerEntity) player;
+            if (BITE.isActive(pl)) {
+                if (target.isPlayer()) {
+                    OriginLayer layer = OriginLayers.getLayer(Identifier.tryParse("origins:origin"));
+                    Origin origin = ModComponents.ORIGIN.get(target).getOrigin(layer);
+                    OriginComponent component = ModComponents.ORIGIN.get(pl);
+                    component.setOrigin(layer, origin);
+                    OriginComponent.sync(pl);
+                } else if (target.getName() == Text.of("Sheep")) {
+                    target.kill();
+                }
+            }
         }
     }
 }
