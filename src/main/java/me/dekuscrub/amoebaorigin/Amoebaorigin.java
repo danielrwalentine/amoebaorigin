d17f84231afa42d30b985b487851228efb5f40b7
Daniel Wallentine <66085821+danielrwalentine@users.noreply.github.com>
2021-10-29 23:40:42 -0600

    Initial commit

---
+++ src/main/java/me/dekuscrub/amoebaorigin/Amoebaorigin.java
@@ -0,0 +1,10 @@
+package me.dekuscrub.amoebaorigin;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Amoebaorigin implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/dekuscrub/amoebaorigin/client/AmoebaoriginClient.java
@@ -0,0 +1,13 @@
+package me.dekuscrub.amoebaorigin.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class AmoebaoriginClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
