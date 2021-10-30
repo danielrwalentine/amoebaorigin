b5d8590c4ada5438e990c65d351bc87c08128e06
Daniel Wallentine <66085821+danielrwalentine@users.noreply.github.com>
2021-10-30 10:31:14 -0600

    Fixed Mixin

--- src/main/java/me/dekuscrub/amoebaorigin/Amoebaorigin.java
+++ src/main/java/me/dekuscrub/amoebaorigin/Amoebaorigin.java
@@ -2,17 +2,19 @@ package me.dekuscrub.amoebaorigin;
 
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 
 public class Amoebaorigin implements ModInitializer {
+
     @Override
     public void onInitialize() {
     }
 
-    public static void bitePlayer(Entity player, Entity target) {
+    public static void bitePlayer(LivingEntity player, Entity target) {
         if (player.isPlayer()) {
-            PlayerEntity player1 = (PlayerEntity) player;
-            player1.jump();
+            player.kill();
+            target.kill();
 
         }
     }
--- src/main/java/me/dekuscrub/amoebaorigin/mixin/BiteMixin.java
+++ src/main/java/me/dekuscrub/amoebaorigin/mixin/BiteMixin.java
@@ -2,19 +2,19 @@ package me.dekuscrub.amoebaorigin.mixin;
 
 import me.dekuscrub.amoebaorigin.Amoebaorigin;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Objects;
 
-@Mixin(Entity.class)
+@Mixin(LivingEntity.class)
 public class BiteMixin {
 
-    @Inject(method = "damage", at = @At("HEAD"))
-    private void damage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-        Amoebaorigin.bitePlayer(Objects.requireNonNull(source.getSource()), (Entity)(Object)this);
+    @Inject(method = "onAttacking", at = @At("HEAD"))
+    private void onAttacking(Entity target, CallbackInfo ci) {
+        Amoebaorigin.bitePlayer(Objects.requireNonNull((LivingEntity)(Object)this), target);
     }
 }
